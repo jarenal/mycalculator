569e1b44c7599b3ac7bde5a8a3eecda7c399aa0c
Jose Antonio <jguidos@hotmail.com>
2022-03-16 21:15:51 +0100

    Add frontend logic

--- src/assets/app.js
+++ src/assets/app.js
@@ -6,7 +6,19 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+const $ = require('jquery');
+require('bootstrap');
+
+$(document).ready(function () {
+    $(document).off("click", "#calculator_form_Reset").on("click", "#calculator_form_Reset", function (e) {
+        $("#calculator_form_operand1").val("0");
+        $("#calculator_form_operand2").val("0");
+        $("#calculator_result").val("0");
+        $("#calculator_form_operator option[value='+']").attr("selected", true);
+    })
+})
